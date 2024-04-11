20ae8975a5c2d66e01662e62bf268dc19a6146fe
Sarah Ahmad <saa804@nyu.edu>
2024-04-11 11:08:43 -0400

    Initial commit

---
+++ js/clt-data.js
@@ -0,0 +1,142 @@
+var cltData = [
+    {
+      "communitylandtrust": "Bronx CLT",
+      "Borough": "Bronx",
+      "longitude": -73.89378,
+      "latitude": 40.86848,
+      "land status": "Land Transfer in Process"
+    },
+    {
+      "communitylandtrust": "Mary Mitchell Family and Youth Center",
+      "Borough": "Bronx",
+      "longitude": -73.88726,
+      "latitude": 40.84484,
+      "land status": "Advocating for Land"
+    },
+    {
+      "communitylandtrust": "South Bronx Land and Community Resource Trust",
+      "Borough": "Bronx",
+      "longitude": -73.91514,
+      "latitude": 40.82128,
+      "land status": "Advocating for Land"
+    },
+    {
+      "communitylandtrust": "Mott Haven Port Morris Community Land Stewards",
+      "Borough": "Bronx",
+      "longitude": -73.92952,
+      "latitude": 40.80814,
+      "land status": "Land Transfer in Process"
+    },
+    {
+      "communitylandtrust": "Northern Manhattan CLT",
+      "Borough": "Manhattan",
+      "longitude": -73.9383,
+      "latitude": 40.83765,
+      "land status": "Advocating for Land"
+    },
+    {
+      "communitylandtrust": "East Harlem El Barrio CLT",
+      "Borough": "Manhattan",
+      "longitude": -73.94367,
+      "latitude": 40.79506,
+      "land status": "Has Land"
+    },
+    {
+      "communitylandtrust": "This Land Is Ours CLT",
+      "Borough": "Manhattan",
+      "longitude": -73.98667,
+      "latitude": 40.72719,
+      "land status": "Advocating for Land"
+    },
+    {
+      "communitylandtrust": "Cooper Square CLT",
+      "Borough": "Manhattan",
+      "longitude": -73.99071,
+      "latitude": 40.72691,
+      "land status": "Has Land"
+    },
+    {
+      "communitylandtrust": "Chinatown CLT",
+      "Borough": "Manhattan",
+      "longitude": -73.99863,
+      "latitude": 40.71312,
+      "land status": "Advocating for Land"
+    },
+    {
+      "communitylandtrust": "RAIN",
+      "Borough": "Manhattan",
+      "longitude": -73.98739,
+      "latitude": 40.72595,
+      "land status": "Has Land"
+    },
+    {
+      "communitylandtrust": "Northfield LDC",
+      "Borough": "Staten Island",
+      "longitude": -74.13193,
+      "latitude": 40.63648,
+      "land status": "Advocating for Land"
+    },
+    {
+      "communitylandtrust": "Brooklyn Level Up",
+      "Borough": "Brooklyn",
+      "longitude": -73.93096,
+      "latitude": 40.65458,
+      "land status": "Advocating for Land"
+    },
+    {
+      "communitylandtrust": "Central Brooklyn CLT",
+      "Borough": "Brooklyn",
+      "longitude": -73.91375,
+      "latitude": 40.67152,
+      "land status": "Advocating for Land"
+    },
+    {
+      "communitylandtrust": "East New York CLT",
+      "Borough": "Brooklyn",
+      "longitude": -73.8852,
+      "latitude": 40.67962,
+      "land status": "Land Transfer in Process"
+    },
+    {
+      "communitylandtrust": "Brownsville CLT",
+      "Borough": "Brooklyn",
+      "longitude": -73.91403,
+      "latitude": 40.66994,
+      "land status": "Land Transfer in Process"
+    },
+    {
+      "communitylandtrust": "Ravenswood CLT",
+      "Borough": "Queens",
+      "longitude": -73.93519,
+      "latitude": 40.76202,
+      "land status": "Advocating for Land"
+    },
+    {
+      "communitylandtrust": "Western Queens CLT",
+      "Borough": "Queens",
+      "longitude": -73.93571,
+      "latitude": 40.74493,
+      "land status": "Advocating for Land"
+    },
+    {
+      "communitylandtrust": "Chhaya CDC",
+      "Borough": "Queens",
+      "longitude": -73.88863,
+      "latitude": 40.74796,
+      "land status": "Advocating for Land"
+    },
+    {
+      "communitylandtrust": "ReAL Edemere CLT",
+      "Borough": "Queens",
+      "longitude": -73.78076,
+      "latitude": 40.59447,
+      "land status": "Land Transfer in Process"
+    },
+    {
+      "communitylandtrust": "Interboro CLT",
+      "Borough": "Citywide",
+      "longitude": -74.00722,
+      "latitude": 40.69823,
+      "land status": "Has Land"
+    }
+  ]
---
+++ js/scripts.js
@@ -0,0 +1,53 @@
+
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2FyYWhoYWhtYWQiLCJhIjoiY2x1bHU0NDdxMDBtcTJqb3lwcDAyM3NpMSJ9.LtM9x5jiBhMAt00hdlBVyw';
+
+var mapOptions = {
+    container: 'map-container', // container ID
+    style: 'mapbox://styles/mapbox/dark-v11', // dark basemap
+    center: [-74.05, 40.68], // starting position [lng, lat]
+    zoom: 10.4, // starting zoom,
+}
+
+// instantiate the map
+const map = new mapboxgl.Map(mapOptions);
+
+// add a navitation control
+const nav = new mapboxgl.NavigationControl();
+map.addControl(nav, 'top-right');
+
+// loop over the cltData array to make a marker for each record
+cltData.forEach(function (cltRecord) {
+
+    var color
+
+    // use if statements to assign colors based on cltData.program
+    if (cltRecord.program === 'Advocating for Land') {
+        color = '#C95D63'
+    }
+    if (cltRecord.program === 'Land Transfer in Progress') {
+        color = '#AE8799'
+    }
+    if (cltRecord.program === 'Has Land') {
+        color = '#717EC3'
+    }
+    
+
+
+    // create a popup to attach to the marker
+    const popup = new mapboxgl.Popup({
+        offset: 24,
+        anchor: 'bottom'
+    }).setText(
+        `${cltRecord.communitylandtrust}`
+    );
+
+    // create a marker, set the coordinates, add the popup, add it to the map
+    new mapboxgl.Marker({
+        scale: 0.65,
+        color: color
+    })
+        .setLngLat([cltRecord.longitude, cltRecord.latitude])
+        .setPopup(popup)
+        .addTo(map);
+})
